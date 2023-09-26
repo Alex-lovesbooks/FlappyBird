e7a3b598c88d906655477314874af2f8b0dcd98f
Alexandra <xandra.balint@gmailcom>
2023-09-26 16:50:39 +0300

    initial files

---
+++ bird.js
@@ -0,0 +1,40 @@
+// function Bird(){
+//   this.y = height/2;
+//   this.x = 25;
+//   this.show = function (){
+//     fill(255);
+//     ellipse(this.y, this.x, 16, 16);
+//   }
+// }
+
+function Bird(){
+  this.x = height/2;
+  this.y = 100;
+  this.gravity = 0.1 ;
+  this.lift=-20;
+  this.velocity = 0;
+  this.show = function (){
+    fill(255);
+    ellipse(this.y, this.x, 32, 32);    
+  }
+  this.up = function(){
+    this.velocity +=  this.lift ;
+ 
+  }
+
+  this.update = function(){
+    this.velocity += this.gravity;
+    this.x +=this.velocity;  
+    this.velocity +=0.9; 
+    if(this.x>height-16){
+    this.x = height-16;
+    this.velocity=0;
+  }
+        if(this.x<16){
+    this.x = 16;
+    this.velocity=0;
+  }
+  }
+   
+  
+}
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var pipes =[];   
+function setup() {
+  createCanvas(200, 300);
+  bird = new Bird(); 
+  pipes.push( new Pipe());
+}
+
+function draw() {
+  background(0);
+  bird.update();
+  bird.show();
+  for (var i=pipes.lenght-1; i >=0 ; i--){
+    pipes[i].show();
+    pipes[i].update();
+    console.log("xx");
+  }
+}
+
+function ResetBird(b){
+  b.x=height/2;
+  
+}
+
+function keyPressed(){
+  if (key == 'a'){console.log("space"); 
+      bird.up();}
+  if (key == 'b'){ResetBird(bird);}
+
+}
+          
