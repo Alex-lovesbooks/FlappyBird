184ab70cace4421bb58a93380d672ba9fdcdb591
Alexandra <xandra.balint@gmailcom>
2023-10-01 18:36:35 +0300

    Revert "pipe show"
    This reverts commit 87c78153400735d87e7c7f6226735e44b524a3d2.

--- pipe.js
+++ pipe.js
@@ -1,11 +1,9 @@
-import { pipes } from "./sketch";
-
 function Pipe() {
   this.top = random(height / 2);
   this.bottom = random(height / 2);
   this.y = width;
   this.w = 20;
-  this.speed = 1;
+  this.speed = 5;
 
   this.show = function () {
     fill(255);
@@ -15,16 +13,4 @@ function Pipe() {
   this.update = function () {
     thix.y -= this.speed;
   };
-}
-function draw() {
-  background(0);
-  bird.update();
-  bird.show();
-  if (frameCount % 40 == 0) {
-    pipes.push(new Pipe());
-  }
-  for (var i = 0; i < pipes.lenght; i++) {
-    pipes[i].show();
-    pipes[i].update();
-  }
 }
