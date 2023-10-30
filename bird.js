db67b846cec57964d40b3f68dd320b1c3ee13c3a
Alexandra <xandra.balint@gmailcom>
2023-10-30 11:12:45 +0200

    bird back

--- bird.js
+++ bird.js
@@ -1,14 +1,17 @@
 class Bird {
   constructor() {
-    this.x = height / 2;
-    this.y = 100;
+    this.y = height / 2;
+    this.x = 100;
     this.gravity = 0.01;
     this.lift = -15;
     this.velocity = 0.1;
+    this.score = 0;
+
+    this.radius = 16;
   }
   show() {
     fill(255);
-    ellipse(this.y, this.x, 32, 32);
+    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
   }
 
   up() {
@@ -17,14 +20,14 @@ class Bird {
 
   update() {
     this.velocity += this.gravity / 2;
-    this.x += this.velocity / 2;
+    this.y += this.velocity / 2;
     this.velocity += 0.4;
-    if (this.x > height - 16) {
-      this.x = height - 16;
+    if (this.y > height - this.radius) {
+      this.y = height - this.radius;
       this.velocity = 0;
     }
-    if (this.x < 16) {
-      this.x = 16;
+    if (this.y < this.radius) {
+      this.y = this.radius;
       this.velocity = 0;
     }
   }
