16bb09a974b55550308cba56fa65f769ee00ab59
Alexandra <xandra.balint@gmailcom>
2023-10-30 11:09:07 +0200

    go back to finished game

--- sketch.js
+++ sketch.js
@@ -1,44 +1,61 @@
-var pipes = [];
+var pipes;
+var bird;
+var running;
 function setup() {
   createCanvas(200, 300);
+  init_scene();
+}
+
+function init_scene() {
+  pipes = [];
+  running = true;
+  frameCount = 0;
   bird = new Bird();
   pipes.push(new Pipeup());
   pipes.push(new Pipedown());
 }
 
 function draw() {
-  background(0);
-  if (frameCount % (width / 2) == 0) {
-    pipes.push(new Pipeup());
-    pipes.push(new Pipedown());
-  }
-
-  for (var i = 0; i < pipes.length; i++) {
-    pipes[i].update();
-    pipes[i].show();
-    if (pipes[i].hits() == true) console.log("hit");
-  }
-
-  var to_remove_pipes = [];
-  for (var i = 0; i < pipes.length; i++) {
-    if (pipes[i].outside_screen()) {
-      to_remove_pipes.push(i);
+  if (running == true) {
+    background(0);
+    if (frameCount % (width / 2) == 0) {
+      pipes.push(new Pipeup());
+      pipes.push(new Pipedown());
     }
-  }
-  for (var i = to_remove_pipes.length - 1; i >= 0; i--) {
-    pipes.splice(to_remove_pipes[i], 1);
-  }
 
-  bird.update();
-  bird.show();
+    for (var i = 0; i < pipes.length; i++) {
+      pipes[i].update();
+      if (pipes[i].hits(bird) == true) running = false;
+      pipes[i].show();
+    }
+
+    var to_remove_pipes = [];
+    for (var i = 0; i < pipes.length; i++) {
+      if (pipes[i].outside_screen()) {
+        to_remove_pipes.push(i);
+      }
+    }
+    for (var i = to_remove_pipes.length - 1; i >= 0; i--) {
+      pipes.splice(to_remove_pipes[i], 1);
+    }
+
+    bird.update();
+    bird.show();
+    text(bird.score, 10, 30);
+  } else {
+    text("GAME OVER", width / 3, height / 2);
+    text("Score: " + bird.score, width / 3, height / 2 + 32);
+    text("press Space to to play again", width / 3, height / 2 + 64);
+  }
 }
 
 function ResetBird(b) {
-  b.x = height / 2;
+  b.y = height / 2;
 }
 
 function keyPressed() {
   if (key == " ") {
+    if (running == false) init_scene();
     console.log("space");
     bird.up();
   }
